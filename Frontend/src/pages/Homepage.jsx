b37dccc74422f98d15119ce51938e10b59ff9e48
M4CTAVICH <ouaretchakib@gmail.com>
2025-03-29 22:04:58 +0100

    wiw update

--- Frontend/src/pages/Homepage.jsx
+++ Frontend/src/pages/Homepage.jsx
@@ -15,39 +15,38 @@ const Homepage = () => {
       </header>
       <section className="hero">
         <div className="hero-content">
-            <h1>Manage your tasks Efficiently</h1>
-            <p className="hero-text">stay organized , boost productivity , and never miss a deadline</p>
-            <button className="cta-button">Get started</button>
+          <h1>Manage your tasks Efficiently</h1>
+          <p className="hero-text">
+            stay organized , boost productivity , and never miss a deadline
+          </p>
+          <button className="cta-button">Get started</button>
         </div>
       </section>
       <section id="features" className="features">
         <h2>Key features</h2>
         <div className="features-grid">
-            <div className="feature-card">
-                <h3>Task Oranization</h3>
-                <p>keep your tasks organized and prioritized</p>
-
-            </div>
-            <div className="feature-card">
-                <h3>Progress Tracking</h3>
-                <p>Monitor your progress in real-time</p>
-            </div>
-            <div className="feature-card">
-                <h3>Collaboration</h3>
-                <p>Work together with your team seamless</p>
-
-            </div>
+          <div className="feature-card">
+            <h3>Task Oranization</h3>
+            <p>keep your tasks organized and prioritized</p>
+          </div>
+          <div className="feature-card">
+            <h3>Progress Tracking</h3>
+            <p>Monitor your progress in real-time</p>
+          </div>
+          <div className="feature-card">
+            <h3>Collaboration</h3>
+            <p>Work together with your team seamless</p>
+          </div>
         </div>
       </section>
       <section id="contact" className="contact">
         <h2>Get In Touch</h2>
         <p>Have questions? Reach out to us!</p>
-
       </section>
       <footer className="footer">
-        <p>TaskMaster © {new Date().getFullYear()}</p>
+        <p>WiW © {new Date().getFullYear()}</p>
       </footer>
-    </div> 
+    </div>
   );
 };
 export default Homepage;
